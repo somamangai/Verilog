a6dae4f04ee5eb7c33a544df28e398809c36d23b
Mangai <mangai.knms@gmail.com>
2025-07-28 13:15:25 +0530

    Update vector1.v

--- Basics/Vectors/vector1.v
+++ Basics/Vectors/vector1.v
@@ -1,1 +1,13 @@
+//Build a combinational circuit that splits an input half-word (16 bits, [15:0] ) into lower [7:0] and upper [15:8] bytes.
+/*
+Implicit nets are often a source of hard-to-detect bugs. In Verilog, net-type signals can be implicitly created by an assign statement or by attaching something undeclared to a module port. Implicit nets are always one-bit wires and causes bugs if you had intended to use a vector. Disabling creation of implicit nets can be done using the `default_nettype none directive.
+*/
+`default_nettype none     // Disable implicit nets. Reduces some types of bugs.
+module top_module( 
+    input wire [15:0] in,
+    output wire [7:0] out_hi,
+    output wire [7:0] out_lo );
 
+    assign out_hi[7:0] = in [15:8];
+    assign out_lo[7:0] = in [7:0];
+endmodule
