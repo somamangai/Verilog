7478f2f171403130cd2a0abea871b2515f34216a
Mangai <mangai.knms@gmail.com>
2025-07-28 14:41:27 +0530

    Update Bugs_nand3.v

--- Basics/Verification_Finding_bugs/Bugs_nand3.v
+++ Basics/Verification_Finding_bugs/Bugs_nand3.v
@@ -3,3 +3,8 @@
     andgate inst1 ( a, b, c, out );
 
 endmodule*/
+module top_module (input a, input b, input c, output out);//
+wire con;
+    andgate inst1 ( con, a, b, c,1,1);
+assign out = !con;
+endmodule
